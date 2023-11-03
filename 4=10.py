377b79fa426d454e233079c06f37e143f904be3e
Enzo <75796536+enzo405@users.noreply.github.com>
2023-11-03 09:29:51 +0100

    Add timer to check program break + add user input to custom number

--- 4=10.py
+++ 4=10.py
@@ -1,51 +1,54 @@
 import random
+import time
 
-def askPrompt() -> list|bool :
+def askPrompt(prompt) -> list|bool :
     try:
-        number_1 = int(input("Write the 1st number : "))
-        number_2 = int(input("Write the 2nd number : "))
-        number_3 = int(input("Write the 3rd number : "))
-        number_4 = int(input("Write the 4th number : "))
-        return [number_1, number_2, number_3, number_4]
-    except:
-        return False
+        input_number = int(input(f"{prompt} : "))
+        return input_number
+    except ValueError:
+        return askPrompt(prompt)
 
-list_number = askPrompt()
-while list_number == False:
-    list_number = askPrompt()
 
+list_number : list = []
 answers : list = []
-list_operator : list = ["*", "/", "+", "-"] 
+list_operator : list = ["*", "/", "+", "-"]
 list_tries : list = []
 number_try = 0
-gap_tries = 0
+start_time = 0
 result = 0
 odd_number : list = [] # [3,5,7,9]
+
 for i in range(3,10):
     if i % 2 != 0:
         odd_number.append(i)
 
+number_number = askPrompt('How much number do you need')
+while number_number < 3:
+    print("Number need to be higher than 3")
+    askPrompt('How much number do you need')
+
+wanted_result = askPrompt('What result do you want')
+for i in range(0,number_number):
+    number = askPrompt(f"Number {i+1}")
+    list_number.append(number)
 
 while True:
     equations : list = []
     random.shuffle(list_number)
-    dict_number : dict = {
-        "number_1": list_number[0],
-        "number_2": list_number[1],
-        "number_3": list_number[2],
-        "number_4": list_number[3]
-    }
-
+    dict_number : dict = {}
+    for i in range(0,len(list_number)):
+        dict_number.update({f"number_{i+1}":list_number[i]})
+    
+    boolParenthese = bool(random.randint(0,1))
     for k,v in dict_number.items():
-        random_number_parenthese = random.randint(0,1)
-        random_number_operator = random.randint(0,len(list_number)-1)
+        random_number_operator = random.randint(0,len(list_operator)-1)
         operator = list_operator[random_number_operator]
 
         equations.append(v)
         if k != list(dict_number.keys())[-1]:
             equations.append(operator)
 
-    if random_number_parenthese == 1:
+    if boolParenthese:
         maxIndexInsertParentheseOpen = 5
         indexInsertParentheseOpen = random.randint(0,maxIndexInsertParentheseOpen)
         if not isinstance(equations[indexInsertParentheseOpen],int):
@@ -70,20 +73,19 @@ while True:
             result = eval(equations_str)
             number_try += 1
             print(f"Try n°{number_try} => {equations_str}")
-            if result == 10:
+            if result == wanted_result:
                 answers.append(equations_str)
         except Exception as e:
             pass
-    else:
-       gap_tries += 1
-    
-    if gap_tries >= 100000:
+        start_time = time.time()
+
+    current_time = time.time()
+    if int(current_time-start_time) > 10: # If the previous equations was found more than 10sec ago the program stop
         break
 
 
 if len(answers) == 0:
-    print(f"You can't build an equation that is equal to 10 with these number => {','.join(str(n) for n in dict_number.values())}")
+    print(f"You can't build an equation that is equal to {wanted_result} with these number => {','.join(str(n) for n in dict_number.values())}")
 else:
-    for answer in answers:
-        print(f"The correct equations is : {answer}")
+    print(f"Here is the first correct equations found : {answers[0]}")
     print(f"There are {len(answers)} answers. Found in {number_try} combination")
